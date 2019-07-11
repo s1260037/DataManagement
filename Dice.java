ba68483e1328906ae1970754a6f4e0a78cad049c
s1260037 <s1260037@u-aizu.ac.jp>
2019-07-11 18:12:54 +0900

    add janken

--- Dice.java
+++ Dice.java
@@ -15,5 +15,12 @@ class Dice{
 	System.out.println("Total value: " + (i + j));
 	if(i+j >= 7)System.out.println(str + " won!");
 	else System.out.println(str + " lost...");
+	System.out.println("rock-paper-scissors!!!!");
+	System.out.println("SELECT!!  1:rock, 2:paper, 3:scissors");
+	int r = s.nextInt();
+	int k = rand.nextInt()%3 + 1;
+	if(r == k) System.out.println(str + " even!");
+	else if((r == 1 && k == 3) || (r == 2 && k == 1) || (r == 3 && k == 2))System.out.println(str + " WIN!!!");
+	else System.out.println(str + " lose...");
     }
 }
