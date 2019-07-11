9c229c352c6271f85472861680ee5811170861fa
s1260037 <s1260037@u-aizu.ac.jp>
2019-07-11 17:36:32 +0900

    add Greeding

--- Dice.java
+++ Dice.java
@@ -1,6 +1,11 @@
 import java.util.Random;
+import java.util.Scanner;
 class Dice{
     public static void main(String[] args){
+	Scanner s = new Scanner(System.in);
+	System.out.println("What your name?");
+	String str = s.nextLine();
+	System.out.println("Hello, "+ str + "!");
 	System.out.println("Rolling the dice...");
 	Random rand = new Random();
 	int i = rand.nextInt(6)%6 + 1;
